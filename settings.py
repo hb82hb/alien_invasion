3584acefde913de11b113d083c3bab3dadd8bfdb
Ben De Bonis <ben.debonis@gmail.com>
2022-06-17 14:57:57 -0700

    Changed ship image, background color 230, 230, 230

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings:
         #Screen settings
         self.screen_width = 1800
         self.screen_height = 1200
-        self.bg_color = (135, 206, 235)
+        self.bg_color = (230, 230, 230)
 
         #Ship settings
         self.ship_speed = 1.5
